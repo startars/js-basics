40ea270b7a6fb1a408496e59acd7ba050f8f9f1c
xuki <igor.marcin.wnuk@gmail.com>
2018-11-05 12:11:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let interestRate = 0.3;
+
+let person = {
+name: 'Xuki',
+age: 36
+};
+
+person.name = 'John';
+
+person['name'] = 'Marry';
+
+//function
+
+function greet(name){
+console.log('Hello'+ name);
+
+}
+greet('John');
